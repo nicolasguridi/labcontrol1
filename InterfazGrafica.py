47236d273d2edb51adaa9741483801001f3f7fca
Nicolás Guridi <nguridi@uc.cl>
2020-08-28 23:24:52 -0400

    Interfaz grafica unificada

--- InterfazGrafica.py
+++ InterfazGrafica.py
@@ -77,6 +77,7 @@ cliente = Cliente("opc.tcp://localhost:4840/freeopcua/server/", suscribir_evento
 cliente.conectar()
 
 # Dash settings
+# https://coolors.co/022b3a-1f7a8c-ef2d56-f1faee-5fad56
 colors = {'background': '#022B3A','text': '#f1faee'}
 fonts = {'text': 'Helvetica'}
 frequency = 1
@@ -84,7 +85,7 @@ frequency = 1
 # Dash layout
 app = dash.Dash()
 app.layout = html.Div(style={'backgroundColor': colors['background'], 'font-family': fonts['text']}, children=[
-                html.H1(children='Aplicación de Control', style={'textAlign': 'center','color': '#022B3A', 'paddingTop': '20px', 'paddingBottom': '10px', 'backgroundColor': '#FFFFFF'}),
+                html.H1(children='Aplicación de Control', style={'textAlign': 'center','color': '#022B3A', 'paddingTop': '10px', 'paddingBottom': '20px', 'backgroundColor': '#FFFFFF'}),
                 dcc.Interval(id='interval-component', interval=int(1/frequency*1000), n_intervals=0),
                 html.H2('Tanques', style={'textAlign': 'center', 'color': colors['text']}),
                 html.Div(id='live-update-text1', style={'textAlign': 'center', 'padding':'15px', 'color': colors['text']}),
@@ -99,62 +100,69 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'font-fami
                 html.Div(id='Modo',style={'textAlign': 'center', 'color': colors['text']}, children=[
                     html.H2('Controlador'),
                     dcc.RadioItems(id='Eleccion',options=[{'label': 'Manual', 'value': 'Manual'}, {'label': 'Automático', 'value': 'Automatico'}], value='Manual'),
-                    html.Div(id='MyDiv')]),
+                    ]),
                 html.Div(id='Modos',className='row' ,children=[
-                    html.Div(id='Manual', className='six columns', style={'color': colors['text'], 'borderStyle': 'solid', 'borderWidth': '5px', 'borderColor': '#1F7A8C'}, children=[
+                    html.Div(id='Manual', className='six columns', style={'color': colors['text'], 'padding':'40px'}, children=[
                         html.H3('Modo Manual', style={'textAlign': 'center'}),
                         html.H4('Valor de las Razones'),
                         html.Div(id='RazonesDiv', className='row', children=[
-                            html.Div(id='Razon1Div', style={'paddingBottom':'50px'},className='six columns', children=[
+                            html.Div(id='Razon1Div', style={'paddingTop':'20px'},className='six columns', children=[
                                 html.Label(id='Razon1Label', children='Razon 1'),
+                                html.Br(),
                                 dcc.Slider(id='Razon1', min=0, max=1, step=0.05, value=0.7)]),
-                            html.Div(id='Razon2Div', style={'paddingBottom':'50px'}, className='six columns', children=[
+                            html.Div(id='Razon2Div', style={'paddingTop':'20px'}, className='six columns', children=[
                                 html.Label(id='Razon2Label',children='Razon 2'),
+                                html.Br(),
                                 dcc.Slider(id='Razon2', min=0, max=1, step=0.05, value=0.6)])
                         ]),
-                        dcc.RadioItems(id='TipoManual', options=[{'label': 'Sinusoide', 'value': 'sinusoide'}, {'label':'Valor Fijo', 'value':'fijo'}], value='fijo'),
-                        html.H4('Sinusoide'),
-                        html.Div(id='Sliders1', className='row', children=[
-                            html.Div(id='Frec', style={'paddingBottom':'50px'}, className='six columns', children=[
-                                html.Label(id='1',children='Frec'), dcc.Slider(id='FrecSlider',min=frequency/25, max=frequency/2, step=0.1, value=frequency/4, vertical=False)]),
-                            html.Div(id='Amp', className='six columns', children=[
-                                html.Label(id='2', children='Amp'), dcc.Slider(id='AmpSlider',min=0.1, max=1, step=0.05, value=1, vertical=False)])]),
-                        html.Div(id='Sliders2', className='row', children=[
-                            html.Div(id='Fase', style={'paddingBottom':'50px'}, className='six columns', children=[
-                                html.Label(id='3', children='Fase'), dcc.Slider(id='FaseSlider',min=0, max=6.28, step=0.1, value=0, vertical=False)]),
-                            html.Div(id='Offset', className='six columns', children=[
-                                html.Label(id='4', children='Offset'), dcc.Slider(id='OffsetSlider',min=1, max=3, step=0.05, value=0, vertical=False)])]),
-                        html.H4('Valor fijo'),
-                        html.Div(style={'textAlign':'center', 'paddingBottom':'10px'},children=[dcc.Input(id='ManualFijo',placeholder='Ingrese un valor entre -1 y 1 ...', type='text', value='0')])
+                        html.H4('Valores fijos'),
+                        html.Div(style={'paddingBottom':'10px'},children=[
+                            html.Table([
+                                html.Tr([
+                                    html.Td('Valvula 1'),
+                                    html.Td(dcc.Input(id='ManualFijo1',placeholder='Ingrese un valor entre 0 y 1 ...', type='text', value='0.5'))
+                                ]),
+                                html.Tr([
+                                    html.Td('Valvula 2'),
+                                    html.Td(dcc.Input(id='ManualFijo2',placeholder='Ingrese un valor entre 0 y 1 ...', type='text', value='0.5'))
+                                ])
+                            ]),
+                        ]),
                     ]),
-                    html.Div(id='Automatico', className='six columns', style={'color': colors['text'], 'borderStyle': 'solid', 'borderWidth': '5px', 'borderColor': '#1F7A8C'}, children=[
+                    html.Div(id='Automatico', className='six columns', style={'color': colors['text'], 'padding':'40px'}, children=[
                         html.H3('Modo Automatico', style={'textAlign': 'center'}),
                         html.H4('SetPoints'),
-                        html.Div(id='SetPoints', className='row', children=[
-                            html.Div(id='Tanque1', className='six columns', children=[
-                                html.Label('SetPoint Tanque 1'), dcc.Input(id='SPT1', placeholder='Ingrese valor', type='text', value='25')]),
-                            html.Div(id='Tanque2', className='six columns', children=[
-                                html.Label('SetPoint Tanque 2'), dcc.Input(id='SPT2', placeholder='Ingrese valor', type='text', value='25')])
-                        ]),
-                        html.H4('Constantes del PID'),
-                        html.Div(id='constantes1', className='row', children=[
-                            html.Div(id='P', className='six columns', children=[
-                                html.Label('Proporcional'),
-                                dcc.Input(id='kp',placeholder='Ingrese un valor', type='text', value='0.1')]),
-                            html.Div(id='I', className='six columns', children=[
-                                html.Label('Integral'),
-                                dcc.Input(id='ki',placeholder='Ingrese un valor', type='text', value='0.1')])]),
-                        html.Div(id='constantes2', className='row',  style={'paddingBottom':'10px'},children=[
-                            html.Div(id='D', className='six columns', children=[
-                                html.Label('Derivativa'),
-                                dcc.Input(id='kd',placeholder='Ingrese un valor', type='text', value='0')]),
-                            html.Div(id='W', className='six columns', children=[
-                                html.Label('Anti wind-up'),
-                                dcc.Input(id='kw',placeholder='Ingrese un valor', type='text', value='0')])])
+                            html.Table([
+                                html.Tr([
+                                    html.Td('Set point Tanque 1'),
+                                    html.Td(dcc.Input(id='SPT1', placeholder='Ingrese valor', type='text', value='25'))
+                                ]),
+                                html.Tr([
+                                    html.Td('Set point Tanque 2'),
+                                    html.Td(dcc.Input(id='SPT2', placeholder='Ingrese valor', type='text', value='25'))
+                                ]), 
+                                html.H4('Constantes del PID'),
+                                html.Tr([
+                                    html.Td('Proporcional'),
+                                    html.Td(dcc.Input(id='kp',placeholder='Ingrese un valor', type='text', value='0.1'))
+                                ]),
+                                html.Tr([
+                                    html.Td('Integral'),
+                                    html.Td(dcc.Input(id='ki',placeholder='Ingrese un valor', type='text', value='0.1'))
+                                ]),
+                                html.Tr([
+                                    html.Td('Derivativo'),
+                                    html.Td(dcc.Input(id='kd',placeholder='Ingrese un valor', type='text', value='0.1'))
+                                ]),
+                                html.Tr([
+                                    html.Td('Anti wind-up'),
+                                    html.Td(dcc.Input(id='kw',placeholder='Ingrese un valor', type='text', value='0.1'))
+                                ]),
+                            ]),
                     ])
                 ]),
-                html.Div(id='AlarmaContainer', style={'paddingTop': '20px', 'paddingBottom': '10px'}, children=[
-                    html.Div(id='Alarma', style={'backgroundColor': '#006400', 'width': '80%', 'height': '70px', 'paddingTop':'25px', 'margin':'auto','borderStyle': 'solid', 'borderWidth': '5px', 'borderColor': '#1F7A8C'}, children=[
+                html.Div(id='AlarmaContainer', style={'padding':'40px'}, children=[
+                    html.Div(id='Alarma', style={'backgroundColor': '#006400', 'width': '80%', 'height': '70px', 'margin':'auto'}, children=[
                         html.H2(id='AlarmaTexto',style={'textAlign':'center', 'color': colors['text'], 'paddingBottom':'40px'}, children=['Alarma Inactiva'])
                     ])
                 ])
@@ -168,11 +176,9 @@ def alarm_color(n):
     if system.event_color != 0:
         color = '#EF2D56'
     else:
-        color = '#8CD867'
+        color = '#5FAD56'
     system.event_color = 0
-
-    style = {'backgroundColor': color, 'font-family': fonts['text'], 'color': colors['text'],  'width': '80%', 'height': '70px', 'paddingTop': '25px', 'margin': 'auto',
-                'borderStyle': 'solid', 'borderWidth': '5px', 'borderColor': '#1F7A8C'}
+    style = {'backgroundColor': color, 'font-family': fonts['text'], 'color': colors['text'],  'width': '80%', 'height': '70px', 'paddingTop': '15px', 'margin': 'auto'}
     return style
 
 # alarm text callback function
@@ -224,14 +230,14 @@ def update_graph(heights):
     system.h4.append(heights['h4'])
 
     # make plot for every tank height
-    plot1 = go.Scatter(x=list(system.ts), y=list(system.h1), name='Tanque1', mode='lines+markers')
-    plot2 = go.Scatter(x=list(system.ts), y=list(system.h2), name='Tanque2', mode='lines+markers')
-    plot3 = go.Scatter(x=list(system.ts), y=list(system.h3), name='Tanque3', mode='lines+markers')
-    plot4 = go.Scatter(x=list(system.ts), y=list(system.h4), name='Tanque4', mode='lines+markers')
+    plot1 = go.Scatter(x=list(system.ts), y=list(system.h1), name='Tanque 1', mode='lines+markers')
+    plot2 = go.Scatter(x=list(system.ts), y=list(system.h2), name='Tanque 2', mode='lines+markers')
+    plot3 = go.Scatter(x=list(system.ts), y=list(system.h3), name='Tanque 3', mode='lines+markers')
+    plot4 = go.Scatter(x=list(system.ts), y=list(system.h4), name='Tanque 4', mode='lines+markers')
 
     # create figure to fit the four plots
     fig = plotly.tools.make_subplots(rows=2, cols=2, vertical_spacing=0.2,
-                                     subplot_titles=('Tanque1', 'Tanque2', 'Tanque3', 'Tanque4'), print_grid=False)
+                                     subplot_titles=('Tanque 1', 'Tanque 2', 'Tanque 3', 'Tanque 4'), print_grid=False)
     fig['layout']['margin'] = {
         'l': 30, 'r': 10, 'b': 30, 't': 30
     }
@@ -248,54 +254,28 @@ def update_graph(heights):
 
     return fig
 
-################################################# Control ##############################################################
-# chosen method text callback function
-@app.callback(Output(component_id='MyDiv', component_property='children'),
-    [Input(component_id='Eleccion', component_property='value')])
-def update_output_div(input_value):
-    return f'Ha seleccionado el modo: {input_value}'
 
-#################### Modo Manual ##################################
-# frequency value callback function
-@app.callback(Output('1', 'children'), [Input('FrecSlider', 'value')])
-def update_label_1(value):
-    return f'Frec: {value} Hz'
-# amplitude value callback function
-@app.callback(Output('2', 'children'), [Input('AmpSlider', 'value')])
-def update_label_2(value):
-    return f'Amp: {value}'
-# phase value callback function
-@app.callback(Output('3', 'children'), [Input('FaseSlider', 'value')])
-def update_label_3(value):
-    return f'Fase: {value}'
-# offset value callback function
-@app.callback(Output('4', 'children'), [Input('OffsetSlider', 'value')])
-def update_label_4(value):
-    return f'Offset: {value}'
-# rate 1 value callback function
+# ratio 1 value callback function
 @app.callback(Output('Razon1Label', 'children'), [Input('Razon1', 'value')])
 def update_rate_1(value):
     return f'Razon 1: {value}'
-# rate 2 value callback function
+# ratio 2 value callback function
 @app.callback(Output('Razon2Label', 'children'), [Input('Razon2', 'value')])
 def update_rate_2(value):
     return f'Razon 2: {value}'
 
 
-#################### Modo Automático ##############################
 # pid controller updating and output plotting
 @app.callback(Output('live-update-graph2', 'figure'),
               [Input('intermediate', 'children')],
-              [State('Eleccion', 'value'), State('TipoManual', 'value'),
-                State('FrecSlider', 'value'),State('AmpSlider', 'value'),
-                State('OffsetSlider', 'value'),State('FaseSlider', 'value'),
-                State('ManualFijo', 'value'), State('kp', 'value'),
+              [State('Eleccion', 'value'),
+                State('ManualFijo1', 'value'), State('ManualFijo2', 'value'),
+                State('kp', 'value'),
                 State('ki', 'value'), State('kd', 'value'), State('kw','value'),
                 State('SPT1', 'value'), State('SPT2', 'value'),
                 State('indicativoGuardar', 'children'), State('Formato', 'value'),
                 State('Razon1', 'value'), State('Razon2', 'value')])
-def controller_output(heights, choice, manual_type, freq, amp, offset, phase, fixed,
-                      kp, ki, kd, kw, SPT1, SPT2, saving, formatting, rate_1, rate_2):
+def controller_output(heights, choice, fixed1, fixed2, kp, ki, kd, kw, SPT1, SPT2, saving, formatting, rate_1, rate_2):
     heights = json.loads(heights)
     # set current time and save to historical times
     now = datetime.datetime.now()
@@ -305,14 +285,10 @@ def controller_output(heights, choice, manual_type, freq, amp, offset, phase, fi
     cliente.razones['razon1'].set_value(rate_1)
     cliente.razones['razon2'].set_value(rate_2)
 
-    if choice == 'Manual' and manual_type == 'sinusoide':
-        # put a sinusoid on the valve rates 
-        v1 = v2 = amp * np.cos(2 * np.pi * freq * system.sec + phase) + offset
-        system.sec += 1 / frequency
-
-    elif choice == 'Manual' and manual_type == 'fijo':
+    if choice == 'Manual':
         # fix valve rates
-        v1 = v2 = float(fixed)
+        v1 = float(fixed1)
+        v2 = float(fixed2)
 
     elif choice == 'Automatico':
         # pass set points to pid controller
@@ -335,7 +311,7 @@ def controller_output(heights, choice, manual_type, freq, amp, offset, phase, fi
 
         if choice == 'Manual':
             system.memory.append({'time':now,'h1': heights['h1'], 'h2': heights['h2'], 'h3': heights['h3'], 'h4': heights['h4'],
-                            'v1': v1, 'v2':v2, 'modo': '{}-{}'.format(choice, manual_type)})
+                            'v1': v1, 'v2':v2, 'modo': '{}'.format(choice)})
         else:
             system.memory.append(
                 {'time': now, 'h1': heights['h1'], 'h2': heights['h2'], 'h3': heights['h3'], 'h4': heights['h4'],
@@ -372,7 +348,6 @@ def controller_output(heights, choice, manual_type, freq, amp, offset, phase, fi
     fig['layout']['paper_bgcolor'] = colors['background']
     fig['layout']['font']['color'] = colors['text']
 
-    # fig['layout'].update(height=600, width=600, title='Niveles de los Tanques')
     fig.append_trace(plot1, 1, 1)
     fig.append_trace(plot2, 2, 1)
 
